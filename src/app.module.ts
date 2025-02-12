8b217314242e98fad437903757bb163947f215e2
Henriq-Garcia <botzinso@gmail.com>
2025-02-12 11:48:35 -0300

    feat: create system module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './_modules/database/database.module';
 import { UserModule } from './_modules/user/user.module';
 import { CampaignModule } from './_modules/campaign/campaign.module';
+import { SystemModule } from './_modules/system/system.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, CampaignModule]
+  imports: [DatabaseModule, UserModule, CampaignModule, SystemModule]
 })
 export class AppModule {}
