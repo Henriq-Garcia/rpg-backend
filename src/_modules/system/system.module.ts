6569f8144e251a66f8bd482f2e92b927d999f14b
Henriq-Garcia <botzinso@gmail.com>
2025-02-12 12:14:33 -0300

    feat: createSystem service

--- src/_modules/system/system.module.ts
+++ src/_modules/system/system.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SystemService } from './system.service';
 import { SystemController } from './system.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [SystemService],
   controllers: [SystemController]
 })
--- src/_modules/system/system.service.ts
+++ src/_modules/system/system.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import { CreateSystemDto } from './dto/create-system.dto';
 
 @Injectable()
-export class SystemService {}
+export class SystemService {
+
+    constructor(private prisma: PrismaService) {}
+
+    async createSystem(data: CreateSystemDto) {
+        const systemCreateResult = await this.prisma.system.create({ data });
+        const { createdAt, ...system } = systemCreateResult;
+        return system;
+    }
+}
