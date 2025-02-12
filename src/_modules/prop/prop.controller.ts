7995549c32405dd460480731cb66b0e536f27e89
Henriq-Garcia <botzinso@gmail.com>
2025-02-12 14:20:31 -0300

    feat: base prop module

---
+++ src/_modules/prop/prop.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('prop')
+export class PropController {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './_modules/database/database.module';
 import { UserModule } from './_modules/user/user.module';
 import { CampaignModule } from './_modules/campaign/campaign.module';
 import { SystemModule } from './_modules/system/system.module';
+import { PropModule } from './_modules/prop/prop.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, CampaignModule, SystemModule]
+  imports: [DatabaseModule, UserModule, CampaignModule, SystemModule, PropModule]
 })
 export class AppModule {}
